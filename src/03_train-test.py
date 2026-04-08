ba46281c32aa539915dc679208cc6bed430a39bf
eunhui33 <168899101+eunhui33@users.noreply.github.com>
2026-04-07 19:25:27 -0700

    Update 03_train-test.py

--- src/03_train-test.py
+++ src/03_train-test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv(r'C:\Users\3eunh\OneDrive\Desktop\NS\code\data\02_EDA_inf.csv')
+df = pd.read_csv(r'./data/02_EDA_inf.csv')
 
 train_df, test_df = train_test_split(
     df,
@@ -10,5 +10,5 @@ train_df, test_df = train_test_split(
     random_state=42
 )
 
-train_df.to_csv(r'C:\Users\3eunh\OneDrive\Desktop\NS\code\data\03_train.csv', index=False)
-test_df.to_csv(r'C:\Users\3eunh\OneDrive\Desktop\NS\code\data\03_test.csv', index=False)
+train_df.to_csv(r'./data/03_train.csv', index=False)
+test_df.to_csv(r'./data/03_test.csv', index=False)
